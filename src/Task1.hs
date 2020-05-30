7ccdbca21e0e52ebe739198aa1260cc8e16ad23b
komour <komaroff404@gmail.com>
2020-05-31 00:58:10 +0400

    Begin the first task

--- src/Task1.hs
+++ src/Task1.hs
@@ -1,4 +1,22 @@
 module Task1
-  (
+  ( Point(..)
+  , plus
+  , minus
+  , scalarProduct
+  , crossProduct
   ) where
 
+data Point = Point Int Int
+    deriving (Show)
+
+plus :: Point -> Point -> Point
+plus (Point x1 y1) (Point x2 y2) = Point (x1 + x2) $ y1 + y2
+
+minus :: Point -> Point -> Point
+minus (Point x1 y1) (Point x2 y2) = Point (x1 - x2) $ y1 - y2
+
+scalarProduct :: Point -> Point -> Int
+scalarProduct (Point x1 y1) (Point x2 y2) = x1 * x2 + y1 * y2
+
+crossProduct :: Point -> Point -> Int
+crossProduct (Point x1 y1) (Point x2 y2) = x1 * y2 - x2 * y1
