96f17da879974251bfb3b5274777c34579e8c684
komour <komaroff404@gmail.com>
2020-06-22 01:06:45 +0400

    Add probability of the infection with random, but have troubles with initing grid..

--- src/Comonad19.hs
+++ src/Comonad19.hs
@@ -6,7 +6,7 @@ import           Control.Comonad
 import           Control.Concurrent (threadDelay)
 import           Control.Monad      (liftM2)
 import           System.Process     (callCommand)
---import           System.Random
+import           System.Random
 
 data ListZipper a = LZ [a] a [a]
 
@@ -17,13 +17,15 @@ data Status = Incubative
     | Immune
     | Susceptible
 
-data Fellow = Fellow Status Int
+data Fellow = Fellow Status Int StdGen Float
+-- Int field    - condition (iterations rest to change status)
+-- Float field  - random number
 
 instance Show Fellow where
-  show (Fellow Susceptible _) = " "
-  show (Fellow Incubative _)  = "#"
-  show (Fellow Symptomatic _) = "#"
-  show (Fellow Immune _)      = "@"
+  show (Fellow Susceptible _ _ _) = " "
+  show (Fellow Incubative _ _ _)  = "#"
+  show (Fellow Symptomatic _ _ _) = "#"
+  show (Fellow Immune _ _ _)      = "@"
 
 instance Functor ListZipper where
   fmap :: (a -> b) -> ListZipper a -> ListZipper b
@@ -72,12 +74,12 @@ listWrite :: a -> ListZipper a -> ListZipper a
 listWrite x (LZ ls _ rs) = LZ ls x rs
 
 up, down :: Grid a -> Grid a
-up   (Grid g) = Grid (listLeft  g)
-down (Grid g) = Grid (listRight g)
+up   (Grid g) = Grid $ listLeft  g
+down (Grid g) = Grid $ listRight g
 
 left, right :: Grid a -> Grid a
-left  (Grid g) = Grid (fmap listLeft  g)
-right (Grid g) = Grid (fmap listRight g)
+left  (Grid g) = Grid $ fmap listLeft  g
+right (Grid g) = Grid $ fmap listRight g
 
 gridRead :: Grid a -> a
 gridRead (Grid g) = extract $ extract g
@@ -93,9 +95,9 @@ horizontal = genericMove left right
 vertical   = genericMove up   down
 
 isInfect :: Fellow -> Bool
-isInfect (Fellow Incubative _)  = True
-isInfect (Fellow Symptomatic _) = True
-isInfect (Fellow _ _)           = False
+isInfect (Fellow Incubative _ _ _)  = True
+isInfect (Fellow Symptomatic _ _ _) = True
+isInfect Fellow {}                  = False
 
 infectCount :: [Fellow] -> Int
 infectCount = length . filter isInfect
@@ -106,51 +108,81 @@ neighbours = horizontals ++ verticals ++ liftM2 (.) horizontals verticals
         verticals   = [up, down]
 
 infectNeighbours :: Grid Fellow -> Int
-infectNeighbours g = infectCount
-                  $ map (\direction -> extract $ direction g) neighbours
+infectNeighbours g = infectCount $ map (\direction -> extract $ direction g) neighbours
 
-rule :: Double -> Int -> Int -> Int -> Grid Fellow -> Fellow
-rule p incub symptom immune g = case infectNeighbours g of -- TODO
+fellowPredicate :: Float -> Fellow -> Bool
+fellowPredicate p (Fellow Incubative _ _ n)  = n <= p
+fellowPredicate p (Fellow Symptomatic _ _ n) = n <= p
+fellowPredicate _ Fellow {}                  = False
+
+decideInfection :: Float -> Grid Fellow -> Bool
+decideInfection p g = any (fellowPredicate p . (\ direction -> extract $ direction g)) neighbours
+
+rule :: Float -> Int -> Int -> Int -> Grid Fellow -> Fellow
+rule p incub symptom immune g = case infectNeighbours g of
      0 -> case extract g of
-            Fellow Susceptible x -> Fellow Susceptible x
-            Fellow Immune 0      -> Fellow Susceptible 0
-            Fellow Immune x      -> Fellow Immune $ x - 1
-            Fellow Incubative 0  -> Fellow Symptomatic symptom
-            Fellow Incubative x  -> Fellow Incubative $ x - 1
-            Fellow Symptomatic 0 -> Fellow Immune immune
-            Fellow Symptomatic x -> Fellow Symptomatic $ x - 1
+            Fellow Susceptible x r n -> Fellow Susceptible x r n
+            Fellow Immune 0 r n      -> Fellow Susceptible 0 r n
+            Fellow Immune x r n      -> Fellow Immune (x - 1) r n
+            Fellow Incubative 0 r _  -> let (a, nr) = getRandPair r in Fellow Symptomatic symptom nr a
+            Fellow Incubative x r _  -> let (a, nr) = getRandPair r in Fellow Incubative (x - 1) nr a
+            Fellow Symptomatic 0 r n -> Fellow Immune immune r n
+            Fellow Symptomatic x r _ -> let (a, nr) = getRandPair r in Fellow Symptomatic (x - 1) nr a
      _ -> case extract g of
-            Fellow Susceptible _ -> Fellow Incubative incub
-            Fellow Immune 0      -> Fellow Susceptible 0
-            Fellow Immune x      -> Fellow Immune $ x - 1
-            Fellow Incubative 0  -> Fellow Symptomatic symptom
-            Fellow Incubative x  -> Fellow Incubative $ x - 1
-            Fellow Symptomatic 0 -> Fellow Immune immune
-            Fellow Symptomatic x -> Fellow Symptomatic $ x - 1
+            Fellow Immune 0 r n      -> Fellow Susceptible 0 r n
+            Fellow Immune x r n      -> Fellow Immune (x - 1) r n
+            Fellow Incubative 0 r _  -> let (a, nr) = getRandPair r in Fellow Symptomatic symptom nr a
+            Fellow Incubative x r _  -> let (a, nr) = getRandPair r in Fellow Incubative (x - 1) nr a
+            Fellow Symptomatic 0 r n -> Fellow Immune immune r n
+            Fellow Symptomatic x r n -> Fellow Symptomatic (x - 1) r n
+            Fellow Susceptible x r n -> if decideInfection p g then
+                                            let (a, nr) = getRandPair r in Fellow Incubative incub nr a
+                                        else
+                                            Fellow Susceptible x r n
 
-evolve :: Double -> Int -> Int -> Int -> Grid Fellow -> Grid Fellow
+updFellow :: Fellow -> Fellow
+updFellow (Fellow s c g _) = let (a, ng) = getRandPair g in Fellow s c ng a
+
+getRandPair :: StdGen -> (Float, StdGen)
+getRandPair = randomR (0, 1 :: Float)
+
+evolve :: Float -> Int -> Int -> Int -> Grid Fellow -> Grid Fellow
 evolve p inc sym imm = extend $ rule p inc sym imm
 
-initLZ :: ListZipper Fellow
-initLZ = genericMove id id (Fellow Susceptible 0)
+initLZ :: IO (ListZipper Fellow)
+initLZ = do
+  rand <- getStdGen
+  let (a, gen) = getRandPair rand
+  return $ genericMove updFellow updFellow (Fellow Susceptible 0 gen a) -- TODO ?
 
-initGrid :: Grid Fellow
-initGrid = Grid $ duplicate initLZ
+listFellow :: ListZipper Fellow -> ListZipper Fellow
+listFellow  (LZ a b c) = LZ (fmap updFellow a) (updFellow b) $ fmap updFellow c
 
-initGridWithInfectCenter :: Int -> Grid Fellow
-initGridWithInfectCenter k = gridWrite (Fellow Incubative k) initGrid
+duplicateFellow :: ListZipper Fellow -> ListZipper (ListZipper Fellow)
+duplicateFellow = genericMove listFellow listFellow
+
+initGridWithInfectCenter :: Int -> IO (Grid Fellow)
+initGridWithInfectCenter incub = do
+  lz <- initLZ
+  let grid = Grid $ duplicateFellow lz
+  gen <- getStdGen
+  let (a, ng) = getRandPair gen
+  let fellow = updFellow (Fellow Incubative incub ng a)
+  return $ gridWrite (updFellow fellow) grid
 
 gridToList :: Int -> Grid a -> [[a]]
 gridToList n = fmap (toList n) . toList n . unGrid
 
+printList2 :: Show a => [[a]] -> IO ()
+printList2 list2 = putStrLn $ concatMap (\list -> concatMap (\e -> show e ++ " ") list ++ "\n") list2
+
 printGrid :: Show a => Int -> Grid a -> IO ()
-printGrid n g = putStrLn $ concatMap (\list -> concatMap (\e -> show e ++ " ") list ++ "\n") list2
-  where list2 = gridToList n g
+printGrid n g = printList2 $ gridToList n g
 
 toList :: Int -> ListZipper a -> [a]
 toList n (LZ ls x rs) = reverse (take n ls) ++ [x] ++ take n rs
 
-infect :: Double -> Int -> Int -> Int -> Int -> Int -> Grid Fellow -> IO ()
+infect :: Float -> Int -> Int -> Int -> Int -> Int -> Grid Fellow -> IO ()
 infect p inc sym imm iterations size grid = if iterations == 0 then putStrLn ""
   else do
     threadDelay 500000
@@ -158,11 +190,13 @@ infect p inc sym imm iterations size grid = if iterations == 0 then putStrLn ""
     printGrid size grid
     infect p inc sym imm (iterations - 1) size $ evolve p inc sym imm grid
 
-startInfect :: Double -> Int -> Int -> Int -> Int -> Int -> IO ()
-startInfect p inc sym imm iter size = infect p inc sym imm iter size $ initGridWithInfectCenter inc
+startInfect :: Float -> Int -> Int -> Int -> Int -> Int -> IO ()
+startInfect p incub sym imm iter size = do
+  grid <- initGridWithInfectCenter incub
+  infect p incub sym imm iter size grid
 
 sampleInfect :: IO ()
-sampleInfect = startInfect 0.5 1 1 3 20 10
+sampleInfect = startInfect 0.35 1 1 3 20 10
 
 sample :: Int -> IO ()
-sample x = startInfect 0.5 1 1 3 x 10
+sample x = startInfect 0.35 1 1 3 x 10
